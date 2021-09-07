6d8d961762f5661266dea80adc0ac219f6a77426
alfer91 <dasungwkd@naver.com>
2021-09-07 18:37:46 +0900

    서비스 계층에서의 목록 처리

--- src/test/java/org/zerock/guestbook/service/GuestbookServiceTest.java
+++ src/test/java/org/zerock/guestbook/service/GuestbookServiceTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
+import org.zerock.guestbook.entity.Guestbook;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,7 +16,7 @@ class GuestbookServiceTest {
     @Autowired
     private GuestbookService service;
 
-    @Test
+    /*@Test
     public void testRegister() {
 
         GuestbookDTO guestbookDTO = GuestbookDTO.builder()
@@ -23,5 +26,25 @@ class GuestbookServiceTest {
                 .build();
 
         System.out.println(service.register(guestbookDTO));
+    }*/
+
+    @Test
+    public void testList() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL: "+resultDTO.getTotalPage());
+
+        System.out.println("------------------------------------");
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("====================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
 }
