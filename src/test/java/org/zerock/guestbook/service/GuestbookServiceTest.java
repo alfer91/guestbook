415cdb5a1e04621d2a00700e6bbc6db1baf1d9ee
alfer91 <dasungwkd@naver.com>
2021-09-08 15:53:59 +0900

    검색 처리

--- src/test/java/org/zerock/guestbook/service/GuestbookServiceTest.java
+++ src/test/java/org/zerock/guestbook/service/GuestbookServiceTest.java
@@ -28,7 +28,7 @@ class GuestbookServiceTest {
         System.out.println(service.register(guestbookDTO));
     }*/
 
-    @Test
+    /*@Test
     public void testList() {
 
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
@@ -46,5 +46,31 @@ class GuestbookServiceTest {
 
         System.out.println("====================================");
         resultDTO.getPageList().forEach(i -> System.out.println(i));
+    }*/
+
+    @Test
+    public void testSearch() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .type("tc")
+                .keyword("1")
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL: "+resultDTO.getTotalPage());
+
+        System.out.println("-----------------------------------");
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("====================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+
     }
 }
